ebe82f1c3eebaf9dfcdae30309fdfed6b075e0a2
blangwell <b.langwell@outlook.com>
2020-09-22 17:22:45 -0700

    [db] Create models, make migrations, and migrate to polls db

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# var names below become column names in db
+class Question(models.Model):
+  question_text = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+  question = models.ForeignKey(Question, on_delete=models.CASCADE) # fk indicates a 1:m relationship
+  choice_text = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)
